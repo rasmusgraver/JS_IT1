c8987004b68e494b752591703b069fd9a804c598
Rasmus Graver <rasmus.graver@gmail.com>
2025-02-25 16:00:13 +0100

    MontyHall runde 2

--- f MontyHall/script.js
+++ f MontyHall/script.js
@@ -1,1 +1,18 @@
-// Fyll inn her 
+
+const dorer = document.querySelectorAll(".doors > div")
+console.log(dorer)
+
+dorer.forEach(dorDiv => {
+    dorDiv.addEventListener("click", klikkDor)
+});
+
+function klikkDor(event) {
+    console.log(event.target)
+    const dorImg = event.target
+    dorImg.src = "media/semiclosed.png"
+    setTimeout(openDor, 600, dorImg)
+}
+
+function openDor(dorImg) {
+    dorImg.src = "media/open.png"
+}
