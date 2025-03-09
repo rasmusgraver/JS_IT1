b9bdd230f81e97cd57fe20ca38b948bddbff9ee6
Rasmus Graver <rasmus.graver@gmail.com>
2025-03-09 22:34:29 +0100

    Update script4_dadjoke.js

--- g API/script4_dadjoke.js
+++ g API/script4_dadjoke.js
@@ -1,19 +1,4 @@
 const inputElm = document.getElementById("number")
 const output = document.getElementById("output")
 
-async function klikkKnapp() {
-    const tallet = inputElm.value
-
-    const response = await fetch(
-        //  "http://numbersapi.com/" + tall + "/trivia?json"
-        "https://icanhazdadjoke.com/",
-        {
-            headers: {
-                Accept: "application/json",
-            },
-        }
-    )
-    const data = await response.json()
-    console.log(data)
-    output.innerHTML = data.joke
-}
+// TODO: Bruk APIet til dadjokes og hent en tilfeldig dadjoke
