894d909b5368d594c668a9617e194045587944c2
Rasmus Graver <rasmus.graver@gmail.com>
2025-03-11 12:57:40 +0100

    Update script4_dadjoke.js

--- g API/script4_dadjoke.js
+++ g API/script4_dadjoke.js
@@ -1,1 +1,24 @@
 // TODO: Bruk dad joke APIet fra https://icanhazdadjoke.com/api
+const containerElm = document.querySelector(".container")
+
+containerElm.addEventListener("click", () => {
+    console.log("Trykket på container")
+
+    fetch("https://icanhazdadjoke.com/",
+        {
+            headers: {
+                Accept: "application/json",
+            },
+        }
+    )
+        .then((response) => {
+            console.log(response)
+            return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+            containerElm.innerHTML = data.joke
+        })
+
+})
+
